c84c2cc872639b6bc31948f5104923c2899c4525
SophiaMT21 <soh.moraistorres@gmail.com>
2020-11-02 15:52:00 -0300

    relacionamente entre tabelas

--- src/database/db.js
+++ src/database/db.js
@@ -3,6 +3,28 @@ Database.open(__dirname + '/database.sqlite').then(execute)
 
 function execute(db) {
   db.exec(`
-  
-  `)
+  CREATE TABLE IF NOT EXISTS proffys (
+    id INTEGER PRIMARY KAY AUTOINCREMENT,
+    name TEXT,
+    avatar TEXT,
+    whatsapp TEXT,
+    bio TEXT
+  );
+
+  CREATE TABLE IF NOT EXISTS classes (
+    id INTEGER PRIMARY KAY AUTOINCREMENT,
+    subject TEXT,
+    cost TEXT,
+    proffy_id INTEGER
+    );
+
+    CREATE TABLE IF NOT EXISTS class_schedule (
+      id INTEGER PRIMARY KAY AUTOINCREMENT,
+      class_id INTEGER,
+      weekday INTEGER,
+      time_from INTEGER,
+      time_to INTEGER
+      );
+      
+    `)
 }
